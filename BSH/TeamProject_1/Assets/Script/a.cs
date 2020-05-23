a11b36208de843c2c0b1f0ae6d698ac349df223c
BaeSeongHan <qotjdgks7429@naver.com>
2020-05-23 19:10:55 +0900

    BSH
    ....

---
+++ BSH/TeamProject_1/Assets/Script/a.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a : MonoBehaviour
+{
+    public GameObject main;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            main.SetActive(true);
+            
+        }
+    }
+}
---
+++ BSH/TeamProject_1/Assets/Script/change_scene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class change_scene : MonoBehaviour
+{
+
+    public void load_main()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void load_Test()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void load_Test_2()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
