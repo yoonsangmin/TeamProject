5d047fd944fcc26a09d68d34f6d258cc0e0d102c
songchangmin1 <kkim2316@naver.com>
2020-05-30 23:08:57 +0900

    wow

---
+++ gameui/Assets/script/sound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound : MonoBehaviour
+{
+    public AudioClip soundclip;
+
+    // Start is acalled before the first frame update
+    void Start()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+  void playse()
+
+    {
+
+        GetComponent<AudioSource>().clip = soundclip;
+        GetComponent<AudioSource>().Play();
+    }
+}
