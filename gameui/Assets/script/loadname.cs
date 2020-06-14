c1b69421f797b084e19539eeebe2b48bf52889b2
songchangmin1 <kkim2316@naver.com>
2020-06-14 22:42:29 +0900

    ui
    수정

---
+++ gameui/Assets/script/loadname.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class loadname : MonoBehaviour
+{
+
+    public string thename;
+    public GameObject inputfield;
+    public GameObject textdisplay;
+
+
+    // Start is called before the first frame update
+    public void storename()
+    {
+        thename = inputfield.GetComponent<Text>().text;
+        textdisplay.GetComponent<Text>().text = thename +  "님";
+    }
+
+
+}
