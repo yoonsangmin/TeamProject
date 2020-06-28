8f63b7dcdd6d3b9fd36f70fe4bb1e1c9e310e255
songchangmin1 <kkim2316@naver.com>
2020-06-29 06:52:21 +0900

    ui 추가수정
    ㅎㅇ

---
+++ BSH/TP_03/Assets/wide_mode.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wide_mode : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Screen.SetResolution(854, 480, true);
+        Screen.fullScreen = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
